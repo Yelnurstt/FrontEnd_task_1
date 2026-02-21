6b0c31bc1515362b2956fa78901445059b998030
yelnurst <sailaubekov.yelnur@gmail.com>
2026-02-22 03:16:12 +0500

    feat: add Header component

---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>🌾 AgroMarket</h1>
+      <p>Fresh from the farm to your table</p>
+    </header>
+  );
+}
+
+export default Header;
